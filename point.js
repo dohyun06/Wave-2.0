b4c2b8f5fc198d6ddf63813efa28f6b0ae3a5433
dohyun06 <hyun4you2006@naver.com>
2022-05-08 10:53:15 +0900

    Update Wave 2.1.0

--- point.js
+++ point.js
@@ -1,102 +1,84 @@
 export class Point {
-    /* 가장 처음 시작되는 함수 */
     constructor(index, x, y, ordinal) {
-        this.x = x; /* 기준점의 x값 */
-        this.y = y; /* 기준점의 y값 */
-        this.fixedY = y; /* 기준 y값 (화면 높이의 중앙 값) */
-        this.index = index;
-        this.cur = index * 6; /* 파도 번호 + 파도의 기준점 번호 */
+        this.x = x;
+        this.y = y;
+        this.centerY = y;
+        this.fixedY = y;
+        this.prevFixedY = this.fixedY;
+        this.index = index + 1;
+        this.cur = (index + 1) * 6;
         this.max = y;
-        this.maxRatio = y / 12;
-        this.moveRatio = 0;
-        this.moveCnt = 0;
-        this.waveCnt = 0;
-        this.wave = true;
-        this.onClick = null;
+        this.moveRatio = (this.max * 2) / 210;
+        this.wave = null;
         this.autoCur = ordinal;
         this.speed = 0.1;
         this.autoMax = 0;
 
-        document.addEventListener('mousedown', this.mouseDown.bind(this), false);
-        document.addEventListener('mouseup', () => (this.onClick = false), false);
-    }
-
-    mouseDown(e) {
-        this.reset();
-        this.onClick = true;
         document.addEventListener('mousemove', this.mouseMove.bind(this), false);
-        this.mouseMove(e);
     }
 
     mouseMove(e) {
-        if (this.onClick === true) {
-            let pointX = e.pageX;
-            let pointY = e.pageY;
+        let pointX = e.pageX;
+        let pointY = e.pageY;
 
-            if (pointY < this.fixedY) {
-                if (this.x < pointX) this.y = this.fixedY - (1 / (pointX - this.x + 300)) * Math.abs(this.fixedY - pointY) * 300;
-                else if (this.x > pointX) this.y = this.fixedY - (1 / (this.x - pointX + 300)) * Math.abs(this.fixedY - pointY) * 300;
+        if (pointY < this.centerY) {
+            if (this.x < pointX) this.fixedY = this.centerY - (1 / (pointX - this.x + 300)) * Math.abs(this.centerY - pointY) * 300;
+            else if (this.x > pointX) this.fixedY = this.centerY - (1 / (this.x - pointX + 300)) * Math.abs(this.centerY - pointY) * 300;
 
-                this.max = this.fixedY - this.y;
-                this.wave = false;
-            } else {
-                if (this.x < pointX) this.y = this.fixedY + (1 / (pointX - this.x + 300)) * Math.abs(this.fixedY - pointY) * 300;
-                else if (this.x > pointX) this.y = this.fixedY + (1 / (this.x - pointX + 300)) * Math.abs(this.fixedY - pointY) * 300;
-
-                this.wave = true;
-                this.max = this.y - this.fixedY;
+            if (this.fixedY < this.prevFixedY) {
+                this.prevFixedY = this.fixedY;
+                this.max = this.centerY - this.prevFixedY;
             }
 
-            this.moveRatio = (this.max * 2) / 12;
-            this.maxRatio = this.max / 17;
+            if (this.y > this.fixedY) this.prevFixedY = this.centerY;
+
+            this.wave = true;
+        } else {
+            if (this.x < pointX) this.fixedY = this.centerY + (1 / (pointX - this.x + 300)) * Math.abs(this.centerY - pointY) * 300;
+            else if (this.x > pointX) this.fixedY = this.centerY + (1 / (this.x - pointX + 300)) * Math.abs(this.centerY - pointY) * 300;
+
+            if (this.fixedY > this.prevFixedY) {
+                this.prevFixedY = this.fixedY;
+                this.max = this.prevFixedY - this.centerY;
+            }
+
+            if (this.y < this.fixedY) this.prevFixedY = this.centerY;
+
+            this.wave = false;
         }
+
+        this.moveRatio = (this.max * 2) / 6;
     }
 
-    /* 기준점 위치 업데이트 함수 */
     update() {
-        if (this.onClick === false && this.cur === 0) {
-            if (this.wave === true) {
-                this.y -= this.moveRatio;
-                this.moveCnt++;
+        if (this.wave === true) {
+            this.y -= this.moveRatio;
 
-                if (this.moveCnt === 12) {
-                    this.moveCnt = 0;
-                    this.max -= this.maxRatio;
-                    this.moveRatio = (this.max * 2) / 12;
-                    this.waveCnt++;
-                    this.wave = false;
-                }
-            } else if (this.wave === false) {
-                this.y += this.moveRatio;
-                this.moveCnt++;
-
-                if (this.moveCnt === 12) {
-                    this.moveCnt = 0;
-                    this.max -= this.maxRatio;
-                    this.moveRatio = (this.max * 2) / 12;
-                    this.waveCnt++;
-                    this.wave = true;
-                }
+            if (this.y <= this.centerY - this.max) {
+                this.max /= 1.3;
+                this.moveRatio = (this.max * 2) / 10;
+                this.wave = false;
             }
+        } else if (this.wave === false) {
+            this.y += this.moveRatio;
 
-            if (this.waveCnt === 17) {
-                this.y = this.fixedY;
-                this.waveCnt = 0;
-                this.onClick = null;
-                this.cur = this.index * 6;
+            if (this.y >= this.centerY + this.max) {
+                this.max /= 1.3;
+                this.moveRatio = (this.max * 2) / 10;
+                this.wave = true;
             }
-        } else if (this.onClick === false && this.cur > 0) this.cur--;
-        else if (this.onClick === null) {
+        }
+
+        if (this.wave != null && this.max < 10) {
+            this.y = this.centerY;
+            this.wave = null;
+            this.cur = this.index * 6;
+        }
+
+        if (this.max < 10) {
             this.autoCur += this.speed;
             if (this.autoMax < 10) this.autoMax += 0.1;
-            this.y = this.fixedY + Math.sin(this.autoCur) * this.autoMax;
+            this.y = this.centerY + Math.sin(this.autoCur) * this.autoMax;
         }
-    }
-
-    reset() {
-        this.cur = this.index * 6;
-        this.moveCnt = 0;
-        this.waveCnt = 0;
-        this.autoMax = 0;
     }
 }
